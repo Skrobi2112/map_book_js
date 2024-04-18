190c8f9b2bfa4f793a131a5cb8141740e809f60e
Skrobi2112 <166720070+Skrobi2112@users.noreply.github.com>
2024-04-18 13:58:56 +0200

    Feat: added create user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crud import read
+from utils.crud import read, create_user
 from models.data import users
 
 
@@ -9,8 +9,11 @@ if __name__ == '__main__':
         print("Menu:")
         print("0. Zakończ program:")
         print("1. Pokaż co u znajomych: ")
+        print("2. Dodaj znajomego: ")
         menu_option:str=input("Wybierz dostępną funkcje z menu: ")
         if menu_option=="0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,11 @@
 def read(users:list[dict])->None:
     for user in users[1:]:
         print(f'Twój znajomy {user["name"]} opublikował: {user["posts"]}')
+
+        def create_user(users: list[dict]) -> None:
+            name: str = input("Podaj imię użytkownia:")
+            surname: str = input("Podaj nazwisko użytkownika:")
+            posts: int = int(input("Podaj liczbę postów:"))
+            user: dict = {"name": name, "surname": surname, "posts": posts}
+            users.append(user)
+
